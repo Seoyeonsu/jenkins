4be957fd898b77519526286df98b365004766585
koeun <koeun21004@naver.com>
2021-10-21 17:26:45 +0900

    http17

--- http.sh
+++ http.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir /web
-sudo cd /web
+cd /web
 sudo yum install -y expat-devel, gcc, gcc-c++
 sudo wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
 sudo tar xvf httpd-2.4.51.tar.gz
@@ -10,7 +10,6 @@ sudo wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo tar xvf apr-util-1.6.1.tar.gz
 sudo wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
 sudo tar xvf pcre-8.45.tar.gz
-
-
+cd /web/apr-1.7.0
 
 
