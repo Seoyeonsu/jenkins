f35bbe104c23516df309dd17ae7b7262ee2f6a48
koeun <koeun21004@naver.com>
2021-10-21 15:58:25 +0900

    http11

--- http.sh
+++ http.sh
@@ -8,4 +8,7 @@ sudo wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 sudo tar xvf apr-1.7.0.tar.gz
 sudo wget https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo tar xvf apr-util-1.6.1.tar.gz
+sudo wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
+
+
 
