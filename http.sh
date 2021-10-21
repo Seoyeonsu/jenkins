b82b1c328a2e1fa5164414e34d3a77c2398d90bf
koeun <koeun21004@naver.com>
2021-10-21 15:53:47 +0900

    http8

--- http.sh
+++ http.sh
@@ -5,4 +5,5 @@ sudo yum install -y expat-devel, gcc, gcc-c++
 sudo wget https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
 sudo tar xvf httpd-2.4.51.tar.gz
 sudo wget https://downloads.apache.org/apr/apr-1.7.0.tar.gz
+sudo tar xvf apr-1.7.0.tar.gz
 
